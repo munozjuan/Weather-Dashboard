025869e2fea7420c326ad6ee4fe930ab0e2c6f92
munozjuan <munoz.juan07@hotmail.com>
2020-11-20 00:23:39 -0500

    added functions of local storage and functions that pull arrays

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-//Function that runs AJAX for the 5 day forecast 
+// Global variable declarations
+var cityList = [];
+var cityname;
 
-async function displayWeather() {
+// local storage functions
+initCityList();
+initWeather();
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=d3b85d453bf90d469c82e650a0a3da26";
 
-    var response = await $.ajax({
-        url: queryURL,
-        method: "GET"
-        console.log(response);
+// This function displays the city entered by the user into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
 
-        var currentWeatherDiv = $("<div class='card-body' id='currentWeather'>");
-        var getCurrentCity = response.name;
-        var date = new Date();
-        var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
-        var getCurrentWeatherIcon = response.weather[0].icon;
-        var displayCurrentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getCurrentWeatherIcon + "@2x.png />");
-        var currentCityEl = $("<h3 class = 'card-body'>").text(getCurrentCity+" ("+val+")");
-        currentCityEl.append(displayCurrentWeatherIcon);
-        currentWeatherDiv.append(currentCityEl);
-        var getTemp = response.main.temp.toFixed(1);
-        var tempEl = $("<p class='card-text'>").text("Temperature: "+getTemp+"° F");
-        currentWeatherDiv.append(tempEl);
-        var getHumidity = response.main.humidity;
-        var humidityEl = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
-        currentWeatherDiv.append(humidityEl);
-        var getWindSpeed = response.wind.speed.toFixed(1);
-        var windSpeedEl = $("<p class='card-text'>").text("Wind Speed: "+getWindSpeed+" mph");
-        currentWeatherDiv.append(windSpeedEl);
-        var getLong = response.coord.lon;
-        var getLat = response.coord.lat;
+// This function pulls the city list array from local storage
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+    
+    renderCities();
+    }
 
-        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=d3b85d453bf90d469c82e650a0a3da26&lat="+getLat+"&lon="+getLong;
-        var uvResponse = await $.ajax({
-            url: uvURL,
-            method: "GET"
-        })})}
